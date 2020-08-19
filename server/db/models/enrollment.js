04a5d8f72e47572d2f2ac2186a44e19a6c0b7f58
Joncitone <jarreola7123@gmail.com>
2020-08-19 12:27:38 -0500

    models setup with assoc and export from index

---
+++ server/db/models/enrollment.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+module.exports = Enrollment
+
+const Enrollment = db.define('enrollment', {
+  classGrade: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  },
+  completionStatus: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
+})
