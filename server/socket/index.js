0215282b3246dbe8496ede297b5452ae7da8e0ca
Khuong <lekhuongy@gmail.com>
2020-08-21 16:32:02 -0500

    small change with socket

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,30 +1,24 @@
-
-
 module.exports = io => {
-
   io.on('connection', socket => {
-    
     const memory = {}
     console.log(`A socket connection to the server has been made: ${socket.id}`)
-    
-    socket.on('login', (name) => {
+
+    socket.on('login', name => {
       memory[socket.id] = name
       console.log('mem ', memory)
       io.emit('roster', memory)
     })
-    socket.on('message', (message) => {
+    socket.on('message', message => {
       io.emit('message', message)
     })
 
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
-    
   })
   const two = io.of('/2')
-  
+
   two.on('connection', socket => {
     socket.emit('message', 'two two')
   })
 }
-
