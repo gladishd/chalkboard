176400d5290c64f043f230701f54409e7c4f5ec5
Khuong <lekhuongy@gmail.com>
2020-08-22 21:43:28 -0500

    moved prototype video code to main under room link

--- server/socket/index.js
+++ server/socket/index.js
@@ -16,9 +16,41 @@ module.exports = io => {
       console.log(`Connection ${socket.id} has left the building`)
     })
   })
+
   const two = io.of('/2')
 
   two.on('connection', socket => {
     socket.emit('message', 'two two')
+  })
+
+  const videoroom = io.of('/video')
+
+  const rooms = {}
+  videoroom.on('connect', socket => {
+    socket.on('join room', roomID => {
+      if (rooms[roomID]) {
+        rooms[roomID].push(socket.id)
+      } else {
+        rooms[roomID] = [socket.id]
+      }
+
+      const otherUser = rooms[roomID].find(id => id !== socket.id)
+      if (otherUser) {
+        socket.emit('other user', otherUser)
+        socket.to(otherUser).emit('user joined', socket.id)
+      }
+
+      socket.on('offer', payload => {
+        videoroom.to(payload.target).emit('offer', payload)
+      })
+
+      socket.on('answer', payload => {
+        videoroom.to(payload.target).emit('answer', payload)
+      })
+
+      socket.on('ice-candidate', incoming => {
+        videoroom.to(incoming.target).emit('ice-candidate', incoming.candidate)
+      })
+    })
   })
 }
