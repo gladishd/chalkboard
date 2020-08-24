e6e4de83fba71616dc85aab437a273b7bb3a7b03
Khuong <lekhuongy@gmail.com>
2020-08-24 14:42:45 -0500

    delete socket for video

--- server/socket/index.js
+++ server/socket/index.js
@@ -23,35 +23,4 @@ module.exports = io => {
   two.on('connection', socket => {
     socket.emit('message', 'two two')
   })
-
-  const videoroom = io.of('/video')
-
-  const rooms = {}
-  videoroom.on('connect', socket => {
-    socket.on('join room', roomID => {
-      if (rooms[roomID]) {
-        rooms[roomID].push(socket.id)
-      } else {
-        rooms[roomID] = [socket.id]
-      }
-
-      const otherUser = rooms[roomID].find(id => id !== socket.id)
-      if (otherUser) {
-        socket.emit('other user', otherUser)
-        socket.to(otherUser).emit('user joined', socket.id)
-      }
-
-      socket.on('offer', payload => {
-        videoroom.to(payload.target).emit('offer', payload)
-      })
-
-      socket.on('answer', payload => {
-        videoroom.to(payload.target).emit('answer', payload)
-      })
-
-      socket.on('ice-candidate', incoming => {
-        videoroom.to(incoming.target).emit('ice-candidate', incoming.candidate)
-      })
-    })
-  })
 }
